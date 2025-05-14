107c7fb9e9af66bae33e550fa2b95270f0f41277
yosuang <noreply.yosuang@gmail.com>
2025-05-15 01:52:19 +0800

    Sync

---
+++ LazyVim/lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+local opt = vim.opt
+
+opt.spell = false
+-- 例如 markdown 中的代码块的标记
+opt.conceallevel = 0
--- LazyVim/options.lua
+++
@@ -1,5 +0,0 @@
-local opt = vim.opt
-
-opt.spell = false
--- 显示隐藏字符，例如 markdown 中的代码块的标记
-opt.conceallevel = 0
